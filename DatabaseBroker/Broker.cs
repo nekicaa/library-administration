99724cbabf0278a8b0b4eda61b393afe43eb109d
nekicaa <nevenakrstic98@gmail.com>
2021-08-16 12:52:49 +0200

    Add project files.

---
+++ DatabaseBroker/Broker.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DatabaseBroker
+{
+    class Broker
+    {
+        private SqlConnection connection;
+        private SqlTransaction transaction;
+
+        public Broker()
+        {
+            connection = new SqlConnection(@"Data Source=DESKTOP-FV8VN8E;Initial Catalog=Seminarski;Integrated Security=True;");
+        }
+
+        public void OpenConnection()
+        {
+            connection.Open();
+        }
+
+        public void CloseConnection()
+        {
+            connection.Close();
+        }
+
+        public void BeginTransaction()
+        {
+            transaction = connection.BeginTransaction();
+        }
+
+        public void Commit()
+        {
+            transaction?.Commit();
+        }
+
+        public void Rollback()
+        {
+            transaction?.Rollback();
+        }
+    }
+}
---
+++ Domen/Autor.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domen
+{
+    [Serializable]
+    public class Autor : IEntity
+    {
+        public int Id { get; set; }
+        public string Ime { get; set; }
+        public string Prezime { get; set; }
+
+        [Browsable(false)]
+        public string TableName => "Autor";
+        [Browsable(false)]
+        public string InsertValues => $"'{Ime}','{Prezime}'";
+        [Browsable(false)]
+        public string IdName => "Id";
+        [Browsable(false)]
+        public string JoinCondition => "";
+        [Browsable(false)]
+        public string JoinTable => "";
+        [Browsable(false)]
+        public string TableAlias => "";
+        [Browsable(false)]
+        public object SelectValues => "*";
+        [Browsable(false)]
+        public string WhereCondition => "";
+        [Browsable(false)]
+        public string GetUpdateValues => "";
+
+        public string Uslov;
+
+        [Browsable(false)]
+        public string GeneralCondition => $"'{Uslov}'";
+        [Browsable(false)]
+        public List<IEntity> GetEntities(SqlDataReader reader)
+        {
+            List<IEntity> result = new List<IEntity>();
+            while (reader.Read())
+            {
+                result.Add(new Autor
+                {
+                    Id = (int)reader[0],
+                    Ime = (string)reader[1],
+                    Prezime = (string)reader[2]
+                });
+            }
+            return result;
+        }
+
+        public override string ToString()
+        {
+            return $"{Ime} {Prezime}";
+        }
+    }
+}
